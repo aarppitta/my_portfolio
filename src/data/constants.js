219227e849caf3ce30bb557d036c95ba5a9bea2f
aarrppiittaa <arpitachaudhari26@gmail.com>
2024-09-02 10:12:06 +0100

    Changing my resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile Front-End Developer, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/aarppitta",
   resume:
-    "hhttps://drive.google.com/file/d/1AIMBGUBE8DmvNzb65iiMSrU1lShUDfpA/view?usp=sharing",
+    "https://drive.google.com/file/d/1AIMBGUBE8DmvNzb65iiMSrU1lShUDfpA/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/rishav_chanda/",
